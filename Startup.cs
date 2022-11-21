65d98fd2efc67f388614135a8eb6cc0d6e54612b
Richard Gilbert <RichardJGilbert@hotmail.com>
2022-11-21 16:27:57 +0000

    azure diagnostics

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using SQLiteFunction.Models;
+using System.IO;
 
 [assembly: FunctionsStartup(typeof(SQLiteFunction.StartUp))]
 namespace SQLiteFunction
@@ -10,9 +11,13 @@ namespace SQLiteFunction
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            File.Copy("D:\\home\\site\\wwwroot\\school.db", "D:\\home\\school.db");
+            File.SetAttributes("D:\\home\\school.db", FileAttributes.Normal);
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseSqlite(Utils.GetSQLiteConnectionString());
+                //options.UseSqlite(Utils.GetSQLiteConnectionString());
+                options.UseSqlite("Data source = D:\\home\\school.db");
             });
         }
 
